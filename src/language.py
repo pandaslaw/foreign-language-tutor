8849f77ea354e421592f5966543cc72a3b1f0d1a
pandaslaw <ludmila.schotkina@gmail.com>
2024-11-05 02:02:01 +0300

    Add enums to specify supported languages and language levels.

---
+++ src/language.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Language(Enum):
+    RUSSIAN = "Русский"
+    ENGLISH = "English"
+    SPANISH = "Español"
+    TURKISH = "Türkçe"
---
+++ src/language_proficiency.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class LanguageProficiency(Enum):
+    BEGINNER = 0
+    INTERMEDIATE = 1
+    ADVANCED = 2
+    FLUENT = 3
