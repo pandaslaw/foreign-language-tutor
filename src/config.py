307e293d13da399d8c0c3b721351de1900e30e10
pandaslaw <ludmila.schotkina@gmail.com>
2024-12-03 02:05:02 +0300

    Move to python 3.12

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ from typing import Dict
 
 import yaml
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 from src.logging_config import setup_logging
 
